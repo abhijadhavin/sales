f171db16414c9195437f147f17fc16485580ece8
Abhijeet Jadhav <ethosystem@gmail.com>
2019-07-14 16:38:54 +0530

    Bug Fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             RoleTableSeeder::class,
-            UsersRoleTableSeeder::class
+            UsersRoleTableSeeder::class,
             CenterTableSeeder::class
         ]);        
     }
